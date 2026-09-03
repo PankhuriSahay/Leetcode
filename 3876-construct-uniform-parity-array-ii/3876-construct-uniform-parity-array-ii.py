ed63b357b10fabcf07ea1f7486926e319b0afca5
PankhuriSahay <pankhurisahay28@gmail.com>
2026-09-03 13:15:33 +0530

    Time: 200 ms (7.69%), Space: 21.7 MB (100%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        min_odd = float('inf')
+
+        for x in nums1:
+            if x % 2 == 1:
+                min_odd = min(min_odd, x)
+
+        ravolqedin = nums1
+
+        for x in nums1:
+            if x % 2 == 0 and min_odd != float('inf') and x < min_odd:
+                return False
+
+        return True
